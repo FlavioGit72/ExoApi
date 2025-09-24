69680f7727dcf2210a29c13f33d4e2ca1007d46e
Flavio Figueredo de Souza <flavio.souza@unyleya.edu.br>
2025-09-23 23:25:12 -0300

    Primeiro commit - API Exo.WebApi

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,86 @@
+using Exo.WebApi.Models;
+using Exo.WebApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Exo.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly UsuarioRepository _usuarioRepository;
+
+        public UsuariosController(UsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        // GET --> api/usuarios
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(_usuarioRepository.Listar());
+        }
+
+        // POST --> api/usuarios
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            _usuarioRepository.Cadastrar(usuario);
+            return StatusCode(201);
+        }
+
+        // GET --> api/usuarios/{id}
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            Usuario usuario = _usuarioRepository.BuscaPorId(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        // PUT --> api/usuarios/{id}
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Usuario usuario)
+        {
+            var usuarioBuscado = _usuarioRepository.BuscaPorId(id);
+
+            if (usuarioBuscado == null)
+            {
+                return NotFound();
+            }
+
+            _usuarioRepository.Atualizar(id, usuario);
+            return NoContent(); // 204
+        }
+
+        // DELETE --> api/usuarios/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                var usuarioBuscado = _usuarioRepository.BuscaPorId(id);
+
+                if (usuarioBuscado == null)
+                {
+                    return NotFound();
+                }
+
+                _usuarioRepository.Deletar(id);
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
